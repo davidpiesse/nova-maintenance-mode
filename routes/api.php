73990adee488154df37a3a72c50ed78ce5befb61
David Piesse <piesse@gmail.com>
2018-08-28 21:01:29 +0100

    Move logic from routes to Controller

--- routes/api.php
+++ routes/api.php
@@ -2,23 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use Davidpiesse\NovaMaintenanceMode\MaintenanceMode;
+use Davidpiesse\NovaMaintenanceMode\Http\Controllers\ToolController;
 
-
-Route::post('up', function (Request $request) {
-    MaintenanceMode::up();
-
-    return response([
-        'message' =>'Application is now live',
-        'currentlyInMaintenance' => app()->isDownForMaintenance(),
-    ], 200);
-});
-
-Route::post('down', function (Request $request) {
-    MaintenanceMode::down($request);
-
-    return response([
-        'message' =>'Application is now in maintenance mode',
-        'currentlyInMaintenance' => app()->isDownForMaintenance(),
-    ], 200);
-});
+Route::post('up', ToolController::class.'@up');
+Route::post('down', ToolController::class.'@down');
--- src/Http/Controllers/ToolController.php
+++ src/Http/Controllers/ToolController.php
@@ -3,13 +3,27 @@
 namespace Davidpiesse\NovaMaintenanceMode\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Davidpiesse\NovaMaintenanceMode\File;
 use Illuminate\Routing\Controller;
+use Davidpiesse\NovaMaintenanceMode\File;
+use Davidpiesse\NovaMaintenanceMode\MaintenanceMode;
 
 class ToolController extends Controller
 {
-    public function index()
-    {
-        // return 'Hello world!';
+    public function up(Request $request){
+        MaintenanceMode::up();
+
+        return response([
+            'message' =>'Application is now live',
+            'currentlyInMaintenance' => app()->isDownForMaintenance(),
+        ], 200);
+    }
+
+    public function down(Request $request){
+        MaintenanceMode::down($request);
+
+        return response([
+            'message' =>'Application is now in maintenance mode',
+            'currentlyInMaintenance' => app()->isDownForMaintenance(),
+        ], 200);
     }
 }
