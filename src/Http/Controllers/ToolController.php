574688a6a4f314791ca790ee51b79c0adfd0f5b1
David Piesse <piesse@gmail.com>
2018-08-28 20:57:00 +0100

    Revert "Merge pull request #1 from tomhatzer/remove_obsolete_controller_class"
    This reverts commit 24e88a8b5a36d4687465ab2b8a8ef3d9cbe7c2b7, reversing
    changes made to 7d8b27e2c5815df554e7c8742ad1dd851da4a0a7.

---
+++ src/Http/Controllers/ToolController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Davidpiesse\NovaMaintenanceMode\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Davidpiesse\NovaMaintenanceMode\File;
+use Illuminate\Routing\Controller;
+
+class ToolController extends Controller
+{
+    public function index()
+    {
+        // return 'Hello world!';
+    }
+}
